027238686266756710ae164e3e33b825a8ff48dd
glopes120 <145976765+glopes120@users.noreply.github.com>
2025-11-27 19:15:07 +0000

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'; // <--- Adiciona isto aqui
 
+// ... resto do código
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
